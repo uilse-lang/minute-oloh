59816b9f8e1d37f96aadd73133a918fe433835b0
uilse-lang <khorolgarav.b0528@gmail.com>
2024-11-16 20:16:09 -0800

    hhe

---
+++ app.js
@@ -0,0 +1,46 @@
+/*//console.log() -> hevleh function
+console.log("hello");
+console.log(5*6/3020*3020);
+console.log("hi"+3);
+console.log("5+5="+(5+5));
+//variable -> huvisagch
+var a=5;
+console.log(a+1);
+var name = "Uils" , age=14;
+console.log("My name is " +name+ " i am "+age+" years old.");
+var a=5; 
+var b=3;
+var p=2024;
+var udur;
+ console.log(a+b);
+console.log("p="+(a+b)*2);
+var nas=prompt("Nas")
+var nas;
+var h=2024;
+alert("tanii tursun on bol "    + (h-nas)) */
+/*var a, b, c, h;
+var a=prompt("Duriin 2 orontoi too")
+var b=Math.floor(a/10);
+console.log("b=" +b);
+var c=a%10;
+console.log("c=" +c);
+var h=b+c;
+console.log("h=" +h)
+alert("2toonii niilber"     + h); */
+/*var a,b,c,h,d;
+var a=prompt("Duriin 3n orontoi too")
+var b=Math.floor(a/100)
+console.log("b=" +b)
+var c=Math.floor(a/10%10)
+console.log("c" +c)
+var h=a%100%10;
+console.log("h=" +h)
+var d=b+c+h;
+console.log("d=" +d)
+alert("3n toonii niilber" +d)*/
+var sec, min, uldegdel;
+var sec=prompt("duriin sekond oruulanuu")
+var min=Math.floor(sec/60);
+console.log(min + "min")
+console.log((sec-(min*60))+ "sec");
+alert("hugatsaa " + min +"min " +(sec-(min*60)) +"sec")
